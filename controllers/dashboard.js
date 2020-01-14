8914451012afd67bc624c31a5bdf1e72ec08eb75
Glitch (substation-staging) <none>
2020-01-14 05:07:12 +0000

    🖐🙇 Finished refactor. Some cleanup TBD

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,33 +1,4 @@
-module.exports = function (app, db) {
-    // csv download
-  app.get("/export", function(request, response) {
-    if (request.session.administrator) {
-      response.setHeader("Content-Type", "text/csv");
-      response.setHeader(
-        "Content-Disposition",
-        'attachment; filename="' + "download-" + Date.now() + '.csv"'
-      );
-
-      request.details = {
-        copy: {
-          title: process.env.TITLE,
-          description: process.env.DESCRIPTION
-        }
-      };
-
-      var subscribers = require(__dirname + '/models/subscribers.js')(app,db);
-      subscribers.getActive(function(err, subs){
-        if (err) {
-          request.details.error = "Braintree error message: " + err.message;
-          response.render("dashboard", request.details);
-        } else {
-          request.details.users = subs;
-          response.render("export", request.details);
-        }
-      });
-    }
-  });
-
+module.exports = function(app, db) {
   // dashboard page:
   app.get("/dashboard", function(request, response) {
     request.details = {
@@ -45,26 +16,27 @@ module.exports = function (app, db) {
         if (request.query.nonce) {
           // returning from a login email
           console.log("admin login attempt: " + request.query.nonce);
-          var auth = require(__dirname + '/../utility/auth.js')(app,db);
-          auth.validateNonce(process.env.ADMIN_EMAIL, request.query.nonce, function(
-            err,
-            data
-          ) {
-            if (data) {
-              request.session.administrator = true;
-              request.details.showadmin = true;
+          var auth = require(__dirname + "/../utility/auth.js");
+          auth.validateNonce(
+            process.env.ADMIN_EMAIL,
+            request.query.nonce,
+            function(err, data) {
+              if (data) {
+                request.session.administrator = true;
+                request.details.showadmin = true;
+              }
+              response.render("dashboard", request.details);
             }
-            response.render("dashboard", request.details);
-          });
+          );
         } else {
           // requesting a login — send a link
-          
-          var mailer = require(__dirname + '/models/messaging.js')(app,db);
+
+          var mailer = require(__dirname + "/../utility/messaging.js");
           mailer.sendToken(
             process.env.ADMIN_EMAIL,
             "Log in link for " + process.env.TITLE,
             "Here you go.",
-            "To log in just gollow this link. It's a one-time link like a password reset, but you never have to worry about a password.",
+            "To log in just follow this link. It's a one-time link like a password reset, but you never have to worry about a password.",
             "Log in now",
             process.env.URL + "dashboard"
           );
@@ -76,4 +48,37 @@ module.exports = function (app, db) {
       }
     }
   });
-}
+
+  // csv download
+  app.get("/export", function(request, response) {
+    request.details = {
+      copy: {
+        title: process.env.TITLE,
+        description: process.env.DESCRIPTION
+      }
+    };
+
+    if (request.session.administrator) {
+      var subscribers = require(__dirname + "/../models/subscribers.js");
+      subscribers.getActive(function(err, subs) {
+        if (err) {
+          console.log("export error");
+          request.details.error = "Braintree error message: " + err.message;
+          response.render("dashboard", request.details);
+        } else {
+          console.log("export download initiated");
+          response.setHeader("Content-Type", "text/csv");
+          response.setHeader(
+            "Content-Disposition",
+            'attachment; filename="' + "download-" + Date.now() + '.csv"'
+          );
+          request.details.users = subs;
+          response.render("export", request.details);
+        }
+      });
+    } else {
+      console.log("export error — not logged in");
+      response.render("dashboard", request.details);
+    }
+  });
+};
--- models/subscribers.js
+++ models/subscribers.js
@@ -1,62 +1,204 @@
-module.exports = function(app, db) {
-  var braintree = require("braintree");
-  var gateway = braintree.connect({
-    environment: braintree.Environment[process.env.BRAINTREE_ENVIRONMENT],
-    merchantId: process.env.BRAINTREE_MERCHANT_ID,
-    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-    privateKey: process.env.BRAINTREE_PRIVATE_KEY
+var braintree = require("braintree");
+var gateway = braintree.connect({
+  environment: braintree.Environment[process.env.BRAINTREE_ENVIRONMENT],
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY
+});
+
+/*******************************************************************
+ *
+ * BEGIN FUNCTIONS
+ *
+ *******************************************************************/
+module.exports.add = function(
+  email,
+  firstName,
+  lastName,
+  nonce,
+  amount,
+  callback
+) {
+  var result = gateway.customer.search(function(search) {
+    search.email().is(email);
   });
+  if (result.success) {
+    // customer already exists. we should check if they're active,
+    // and if not then we sign them up for a new sub at the rate
+    // they selected. if they are...do nothing?
+    // Customer.creditCards
+    //    - each.subscriptions
+    //        - each.planId
+    //        - each.status ("Active")
+    // Customer.paypalAccounts[each]
+    //    - each.subscriptions
+    //        - each.planId
+    //        - each.status ("Active")
+  } else {
+    var finalPrice = process.env.BRAINTREE_MINIMUM_COST;
+    if (amount > finalPrice) {
+      finalPrice = amount;
+    }
 
-  /*******************************************************************
-   *
-   * BEGIN OBJECT FUNCTIONS
-   *
-   *******************************************************************/
-  var add = function() {};
-
-  var getActive = function(callback) {
-    // subscribers.getActive(function(err, subs){...});
-    var users = [
+    gateway.customer.create(
       {
-        firstName: "firstName",
-        lastName: "lastName",
-        email: "email"
-      }
-    ];
-    var stream = gateway.subscription.search(
-      function(search) {
-        search.planId().is(process.env.BRAINTREE_PLAN_ID);
-        search.status().is("Active");
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        paymentMethodNonce: nonce,
+        creditCard: {
+          options: {
+            verifyCard: true
+          }
+        }
       },
-      function(err, subs) {
-        if (err) {
-          callback(err.message, null);
-        } else {
-          var len = subs.length();
-          var count = 0;
-          subs.each(function(err, subscription) {
-            if (subscription.transactions[0].customer.email !== "") {
-              users.push({
-                firstName: subscription.transactions[0].customer.firstName,
-                lastName: subscription.transactions[0].customer.lastName,
-                email: subscription.transactions[0].customer.email
-              });
-              //console.log(JSON.stringify(users));
-              //console.log(subscription.transactions[0].customer.email);
+      function(err, res) {
+        if (res.success) {
+          // res.customer.id; // e.g 160923
+          gateway.subscription.create(
+            {
+              paymentMethodToken: res.customer.paymentMethods[0].token,
+              planId: process.env.BRAINTREE_PLAN_ID,
+              price: finalPrice
+            },
+            function(e, r) {
+              // this is where we find out if it worked and send a response
+              //
+              // TODO/MAILGUN: send welcome email to [request.body.email]
+              //
+              // console.log(JSON.stringify(r));
+              callback(null, 200);
             }
-            count++;
-            if (count == len) {
+          );
+        } else {
+          // Card processing failed. Send a PaymentRequired 402
+          callback(402, null);
+        }
+      }
+    );
+  }
+};
+
+module.exports.getActive = function(callback) {
+  // subscribers.getActive(function(err, subs){...});
+  var users = [
+    {
+      firstName: "firstName",
+      lastName: "lastName",
+      email: "email"
+    }
+  ];
+  var stream = gateway.subscription.search(
+    function(search) {
+      search.planId().is(process.env.BRAINTREE_PLAN_ID);
+      search.status().is("Active");
+    },
+    function(err, subs) {
+      if (err) {
+        callback(err.message, null);
+      } else {
+        var len = subs.length();
+        var count = 0;
+        subs.each(function(err, subscription) {
+          if (subscription.transactions[0].customer.email !== "") {
+            users.push({
+              firstName: subscription.transactions[0].customer.firstName,
+              lastName: subscription.transactions[0].customer.lastName,
+              email: subscription.transactions[0].customer.email
+            });
+            //console.log(JSON.stringify(users));
+            //console.log(subscription.transactions[0].customer.email);
+          }
+          count++;
+          // check if we've read the wole stream. if so, output the CSV
+          if (count == len) {
+            if (count > 0) {
               callback(null, users);
             } else {
               callback("No active subscribers found.", null);
             }
-          });
-        }
+          }
+        });
       }
-    );
-  };
-
-  var remove = function() {};
-
-  var validate = function() {};
+    }
+  );
 };
+
+module.exports.remove = function(email, callback) {
+  // TODO: can't get the customers as an iterable array/object
+  // so we wind up in this nested loopsy upside-down place.
+  // Technically we're fine. There should onle be ONE match for
+  // any given email, but that doesn't feel like a solid or stable
+  // thing to build on. For now it's what we've got.
+  //
+  // We currently iterate through every sub for each customer then
+  // deal with cancelations at the end to avoid race conditions on a
+  // per-user basis.
+  var stream = gateway.customer.search(
+    function(search) {
+      search.email().is(email);
+    },
+    function(err, customers) {
+      if (err) {
+        callback(err, null);
+      } else {
+        console.log("total customers: " + customers.length());
+        customers.each(function(err, customer) {
+          // found at least that this email once belonged to a valid customer
+
+          if (err) {
+            callback(err, null);
+          } else {
+            // we found at least one subscription, but we don't know yet if any are active.
+            // if the nonce is valid and we made it this far it's safe to assume the user
+            // is trying to cancel their subscription, even if they already have, so we show
+            // them success that they are indeed unsubscribed no matter what.
+
+            // loooooooooooooops (credit cards)
+            for (
+              var ii = 0, len = customer.creditCards.length;
+              ii < len;
+              ii++
+            ) {
+              var card = customer.creditCards[ii];
+              //console.log("CARD: " + card.bin);
+              // more loooooooooooooooooooooooops (subscriptions)
+              for (
+                var iii = 0, le = card.subscriptions.length;
+                iii < le;
+                iii++
+              ) {
+                var subscription = card.subscriptions[iii];
+
+                console.log("SUB: " + subscription.id);
+                console.log("SUB PLAN: " + subscription.planId);
+                console.log("SUB STATUS: " + subscription.status);
+
+                if (
+                  subscription.status == "Active" &&
+                  subscription.planId == process.env.BRAINTREE_PLAN_ID
+                ) {
+                  gateway.subscription.cancel(subscription.id, function(
+                    err,
+                    result
+                  ) {
+                    callback(null, result);
+                  });
+                } else {
+                  console.log("subscription already canceled");
+                  // show the success message — this is a funny situation as there's a time
+                  // where the user can cancel their sub but still be seen as 'active' until
+                  // their subscription period ends. better to reassure the user than give a
+                  // false error message if they're trying a second time...
+                  callback(null, true);
+                }
+              }
+            }
+          }
+        });
+      }
+    }
+  );
+};
+
+module.exports.validate = function() {};
